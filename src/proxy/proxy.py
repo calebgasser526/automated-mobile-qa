3befc41e948ea66d869c7498a30260662352577e
Caleb Gasser <cgasser@evolytics.com>
2022-06-13 11:55:59 -0500

    Start to python scripts.

--- src/proxy/proxy.py
+++ src/proxy/proxy.py
@@ -8,11 +8,21 @@ from mitmproxy import ctx
 
 class Counter:
     def __init__(self):
-        self.num = 0
+        pass
 
     def request(self, flow):
-        self.num = self.num + 1
-        ctx.log.info("We've seen %d flows" % self.num)
+        ctx.log("Host: " + flow.request.host)
+        ctx.log("Port: " + str(flow.request.port))
+        ctx.log("Method: " + str(flow.request.method))
+        ctx.log("Scheme: " + str(flow.request.scheme))
+        ctx.log("Path: " + str(flow.request.path))
+        ctx.log("Http Version: " + str(flow.request.http_version))
+        ctx.log("Headers: " + str(flow.request.headers))
+        ctx.log("Content:  " + str(flow.request.content))
+        ctx.log("Trailers: " + str(flow.request.trailers))
+        ctx.log("Timestamp start: " + str(flow.request.timestamp_start))
+        ctx.log("Timestamp end: " + str(flow.request.timestamp_end))
+        ctx.log("Timestamp end: " + str(flow.request.timestamp_end))
 
 
 addons = [
