5e145f14d0b7babcc02755fa9036f8ccd3b475a7
Caleb Gasser <cgasser@evolytics.com>
2022-07-11 10:03:57 -0500

    Update

--- mtest/common/appium_helpers.py
+++ mtest/common/appium_helpers.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def wait_for_element(web_driver, locator, value, timeout_sec=10):
+def wait_for_element(web_driver, locator, value, timeout_sec=30):
     return WebDriverWait(web_driver, timeout_sec).until(EC.presence_of_element_located((locator, value)))
